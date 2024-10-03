c8d7bac637e33a70089a53b9c8509809e0c1fadd
traanfnhaatjminh <minh12a2nvc@gmail.com>
2024-10-04 01:31:43 +0700

    update

---
+++ backend/model/products.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const Product = mongoose.model("product", productSchema);
+
+module.exports = Product;
