eecbbe665a5b52a0b9f8453a069b55bd1e20daac
traanfnhaatjminh <minh12a2nvc@gmail.com>
2024-10-15 01:59:51 +0700

    update tablelist model

--- backend/model/TableList.js
+++ backend/model/TableList.js
@@ -2,17 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const tableList = new Schema({
-    table_id: {
-        type: Number,
-        required: true,
-        unique: true
-    },
     number_of_chair: {
         type: Number,
         required: true
     },
     status: {
-        type: Number,
+        type: Boolean,
         required: true
     }
 });
