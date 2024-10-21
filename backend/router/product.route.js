dddd43ea8fc7e99ee7663c8fec22b5e77aac847f
vmphuong1110 <155510101+vmphuong1110@users.noreply.github.com>
2024-10-21 22:13:08 +0700

    warehouse control

--- backend/router/product.route.js
+++ backend/router/product.route.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const multer = require("multer"); // Import Multer để upload file
 const Product = require("../model/Product");
-const { createNewProduct, getAllProduct, getProductsByCategory } = require("../controllers/models/product-controller");
+const { createNewProduct, getAllProduct, getProductsByCategory, updateProduct, deleteProduct} = require("../controllers/models/product-controller");
 
 const productRouter = express.Router();
 productRouter.use(bodyParser.json());
 
-productRouter.post("/createProduct", createNewProduct);
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads'); // Đường dẫn tới thư mục lưu file upload
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname); // Tạo tên file mới không bị trùng
+    }
+});
+
+const upload = multer({ storage: storage }); 
+
+productRouter.post("/createProduct", upload.single("image"), createNewProduct);
 
 productRouter.get("/list", getAllProduct);
 
 productRouter.get("/getByCategory/:categoryId", getProductsByCategory);
 
+productRouter.put("/updateProduct/:productId", upload.single("image"), updateProduct);
+
+productRouter.delete("/deleteProduct/:productId", deleteProduct);
 module.exports = productRouter;
